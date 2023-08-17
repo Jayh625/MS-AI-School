0a40642584c17f56d16c17f9e9112de4b91bab34
Jayh625 <jayho625@gmail.com>
2023-08-17 17:51:44 +0900

    Python/0817

---
+++ Python/0817/style_transfer_loss.py
@@ -0,0 +1,31 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+
+# ContentLoss
+class ContentLoss(nn.Module) :
+    def __init__(self, target) :
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
+    
+    def forward(self, input) :
+        self.loss = F.mse_loss(input, self.target)
+        return input
+
+# Gram_matrix
+def gram_matrix(input) :
+    a, b, c, d = input.size() # (batch_size, map_number, height, width)
+    features = input.view(a * b, c * d) # 입력 텐서 2d 텐서로 변형 -> 각각의 특성 맵을 벡터 -> 하나의 행렬 합침
+    G = torch.mm(features, features.t()) # 2D 특성 맵을 이용해서 그램 행렬 계산 -> 특성 맵 간의 내적 계산 -> 유사도 측정하는 역할
+    return G.div(a*b*c*d)
+
+# StyleLoss
+class StyleLoss(nn.Module) :
+    def __init__(self, target_feature) :
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+    
+    def forward(self, input) :
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
